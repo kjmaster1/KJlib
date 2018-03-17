3d0c6fa5b5178e05a74ba250e225a4ff88e49949
kjmaster1 <kurtis.clements@hotmail.com>
2018-03-17 12:35:20 +0000

    CoFH StringHelper
    Add CoFH StringHelper for use in inventory generators and future projects.

--- src/main/java/com/kjmaster/kjlib/KJLib.java
+++ src/main/java/com/kjmaster/kjlib/KJLib.java
@@ -1,8 +1,10 @@
 package com.kjmaster.kjlib;
 
+import com.kjmaster.kjlib.common.CommonProxy;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -12,8 +14,13 @@ import org.apache.logging.log4j.Logger;
 public class KJLib
 {
     public static final String MODID = "kjlib";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
+    public static final String CLIENT_PROXY = "com.kjmaster.kjlib.client.ClientProxy";
+    public static final String COMMON_PROXY = "com.kjmaster.kjlib.common.CommonProxy";
     public static Logger LOGGER;
+
+    @SidedProxy(clientSide = KJLib.CLIENT_PROXY, serverSide = KJLib.COMMON_PROXY)
+    public static CommonProxy proxy;
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event) {
